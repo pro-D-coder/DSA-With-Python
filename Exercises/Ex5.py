f2199277c6b54285baf699956b551ed94c2d766a
pro-D-coder <2019pietcsdaksh49@poornima.org>
2021-09-25 15:30:07 +0530

    :construction: Added Exercise %

---
+++ Exercises/Ex5.py
@@ -0,0 +1,18 @@
+'''
+Write a short Python function that takes a positive integer n and returns
+the sum of the squares of all the odd positive integers smaller than n
+'''
+
+
+def small_odd_sum(number):
+    sum = 0
+    if(number < 0):
+        raise ValueError("Must be Positive")
+    for i in range(number-1, 2, -1):
+        if i % 2 != 0:
+            sum += i*i
+    return sum
+
+
+if __name__ == '__main__':
+    print(small_odd_sum(int(input("Enter A Number: "))))
